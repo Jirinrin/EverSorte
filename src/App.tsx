02287e5b69bca12337e4e013ceccf0a82b206264
Jiri Swen <jiri.ei@hotmail.com>
2021-02-12 14:57:30 +0100

    Make it

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC, useState } from 'react';
+import './App.scss';
 
-function App() {
+function convert(list: string): string {
+  console.log(list.split('\n'));
+  return list
+    .split('\n').map(s => s.startsWith('- ') ? s.slice(2) : s)
+    .sort((a,b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+    .join('\n');
+}
+
+const App: FC = () => {
+  const [inp, setInp] = useState('');
+  const [outp, setOutp] = useState('');
+
+  const conv = () => {
+    setOutp(convert(inp));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inoutput">
+        <div>
+          <label>Input</label>
+          <textarea className="input" value={inp} onChange={e => setInp(e.currentTarget.value)} />
+        </div>
+        <div>
+          <label>Output</label>
+          <textarea className="output" value={outp} readOnly onClick={e => e.currentTarget.select()} />
+        </div>
+      </div>
+      <button onClick={conv}>Convert</button>
     </div>
   );
 }
